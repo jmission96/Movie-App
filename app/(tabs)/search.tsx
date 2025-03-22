850f567f8f5e7714d254eefa2605a11f288c071e
Joseph Mission <joseph.mission@bisu.edu.ph>
2025-03-23 00:12:39 +0800

    finished the routing system and added the tabs

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const search = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-primary font-bold">Search Here!</Text>
+    </View>
+  );
+}
+
+export default search
+
+const styles = StyleSheet.create({})
